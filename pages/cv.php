1f8fdef4cc7f2d84d4a8b6442b70c224ddf2c748
Lemufi <59792742+Lemufi@users.noreply.github.com>
2024-04-06 16:01:48 +0200

    Page CV Faite

--- pages/cv.php
+++ pages/cv.php
@@ -1,48 +1,266 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="fr" data-wf-page="5e8a723dc663ab416e6bfe24">
+
 <head>
+    <meta charset="utf-8" />
+    <title>Lemufi</title>
+    <meta content="width=device-width, initial-scale=1" name="viewport" />
 
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php include '../php/config.php' ?>
+    <link href="../images/IMG_20231013_153516_442.jpg" rel="shortcut icon" type="image/x-icon" /> <!-- A Changer -->
 
-    <link rel="stylesheet" href="../css/styles.css">
-    <title><?php echo $pageTitle; ?></title>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <link href="../css/mac.css" rel="stylesheet" type="text/css" />
+    <link href="../css/testimonial.css" rel="stylesheet" type="text/css" />
+    <link href="../css/thumbnail.css" rel="stylesheet" type="text/css" />
+    <link href="../css/responsive.css" rel="stylesheet" type="text/css" />
+    <link href="../css/cv.css" rel="stylesheet" type="text/css" />
 
-    <script src="https://cdn.tailwindcss.com"></script>
-    
+    <link href="https://fonts.googleapis.com" rel="preconnect" />
+    <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin="anonymous" />
+
+    <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js" type="text/javascript"></script>
+    <script src="../js/text.js" type="text/javascript"></script>
 </head>
 
-<body class="bg-gray-100">
+<body>
+    <div data-collapse="medium" data-animation="default" data-duration="400" data-easing="ease" data-easing2="ease"
+        role="banner" class="header w-nav">
+        <div class="w-embed">
+            <style>
+            html {
+                font-size: 62.5%;
+            }
 
-    <!-- Header -->
-    <header class="bg-gray-200 shadow h-16 flex items-center"> <!-- h-16 définit la hauteur du header -->
+            html,
+            body,
+            .site-container {
+                cursor: url('../images/Souris/souris_grise.svg'), auto;
+                cursor: -webkit-image-set(url('../images/Souris/souris_grise.svg') 1x,
+                        url('../images/5e8a415acce5fbb114b18c35_cursor@2x.png') 2x), auto;
+            }
 
-        <nav class="container mx-auto px-6 py-3 flex justify-between w-full">
-            <!-- Logo et nom de la marque -->
-            <a href="/" class="text-2xl font-bold text-black no-underline">
-                <?php echo $brandName; ?>
-            </a>
-            <!-- Navigation -->
-            <div class="flex items-center space-x-6">
-                <a href="projects.php" class="text-xl font-bold text-black transition-colors duration-300 link-underline group">
-                    Mes Projets
-                </a>
-                <a href="cv.php" class="text-xl font-bold text-black transition-colors duration-300 link-underline group">
-                    Curriculum Vitae
-                </a>
-                <a href="veilleTechno.php" class="text-xl font-bold text-black transition-colors duration-300 link-underline group">
-                    Veille Technologique
-                </a>
+            a,
+            .dropdown-toggle,
+            .button {
+                cursor: url('../images/Souris/souris_noire.svg'), auto;
+                cursor: -webkit-image-set(url('../images/Souris/souris_noire.svg') 1x,
+                        url('images/5e8a415acce5fbb114b18c35_cursor@2x.png') 2x), auto;
+            }
+
+            /*width*/
+            .site-container::-webkit-scrollbar {
+                width: 14px;
+            }
+
+            /*track*/
+            .site-container::-webkit-scrollbar-track {
+                background: url("https://assets.website-files.com/5e83fdebae1ad8747ffce684/5e864de332df030389abf5bd_pattern-grey.svg");
+                ;
+                background-size: 6px;
+                border-left: 4px solid #000000;
+            }
+
+            /*thumb*/
+            .site-container::-webkit-scrollbar-thumb {
+                background: #c4c4c4;
+                border: 4px solid #000000;
+                border-right: none;
+            }
+
+            /*thumb hover*/
+            .site-container::-webkit-scrollbar-thumb:hover {
+                background: #ffffff;
+            }
+
+            /*thumb pressed*/
+            .site-container::-webkit-scrollbar-thumb:active {
+                background: #ffffff;
+            }
+
+            .header {
+                width: calc(100% - 16px);
+            }
+
+            .site-container {
+                height: calc(100vh - 68px);
+                overflow-x: hidden;
+                overflow-y: scroll;
+                margin-top: -4px;
+                margin-bottom: -4px;
+            }
+
+            @media screen and (min-width: 768px) {
+                .header {
+                    width: calc(100% - 24px);
+                }
+            }
+
+            @media screen and (min-width: 991px) {
+                .header {
+                    width: calc(100% - 40px);
+                }
+
+                .site-container::-webkit-scrollbar {
+                    width: 24px;
+                }
+ 
+           }
+            </style>
+        </div>
+
+        <!-- Navigation Header -->
+        <a href="/" aria-current="page" class="logo-wrapper w-nav-brand w--current">
+            <img src="../images/IMG_20231013_153516_442.jpg" alt="Website Logo" class="logo" />
+        </a>
+
+        <nav role="navigation" class="nav-menu w-nav-menu">
+            <a href="projects.php" data-w-id="1b0214a5-408b-540b-64df-3835fd7e4438"
+                class="nav-link w-nav-link">Projets</a>
+            <a href="cv.php" data-w-id="1b0214a5-408b-540b-64df-3835fd7e443a" class="nav-link w-nav-link">CV</a>
+            <a href="veilleTechno.php" data-w-id="1b0214a5-408b-540b-64df-3835fd7e443c"
+                class="nav-link w-nav-link">Veille Informatique</a>
+            <div data-hover="true" data-delay="0" class="w-dropdown">
+                <div data-w-id="1b0214a5-408b-540b-64df-3835fd7e4441" class="dropdown-toggle w-dropdown-toggle">
+                    <div>Me Retrouver</div>
+                </div>
+                <nav class="nav-dropdown-list w-dropdown-list">
+                    <a href="https://www.linkedin.com/in/mannyjoubert/" target="_blank"
+                        class="nav-dropdown-link w-dropdown-link">Sur Linkedin</a>
+                    <a href="https://github.com/Lemufi" target="_blank" class="nav-dropdown-link w-dropdown-link">Sur
+                        Github</a>
+                    <a href="https://www.instagram.com/mannyjbr/" target="_blank"
+                        class="nav-dropdown-link w-dropdown-link">Sur Instagram</a>
+                </nav>
             </div>
         </nav>
+        <div class="menu-button w-nav-button">
+            <div class="w-icon-nav-menu"></div>
+        </div>
 
-    </header>
-    <!-- Séparateur noir -->
-    <div class="h-1 bg-black"></div>
+    </div>
 
-    <!-- Le reste du contenu de votre page ici -->
+    <div class="site-container">
+        <div class="section hero-section">
+            <div class="container">
+                <div class="grid-hero">
+                    <div>
+                        <div class="containercv">
+                            <div class="modal-wrapper">
+                                <div class="modal-header">
+                                    <div class="button-circles-wrap"><a href="/" class="button-circle close w-inline-block"></a>
+                                        <div class="button-circle"></div>
+                                    </div>
+                                    <div class="flex-center">
+                                        <div>MannyJoubert</div>
+                                        <div>.html</div>
+                                    </div>
+                                </div>
+                                <div>
+                                    <img class="imagecv" src="../images/MoiLeMoi.jpg" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <h1 class="heading-jumbo">Manny Joubert</h1>
+                        <div class="subhead space-bottom-36">c'est moi !</div>
+                        <a href="projects.php" target="_blank" class="button w-button">Voir Mes Projets</a>
+                    </div>
+                </div>
+            </div>
 
+            <div class="containercv2">
+                <div class="modal-wrapper">
+                    <div class="modal-header">
+                        <div class="button-circles-wrap"><a href="/" class="button-circle close w-inline-block"></a>
+                            <div class="button-circle"></div>
+                        </div>
+                        <div class="flex-center">
+                            <div>MonParcours</div>
+                            <div>-project.html</div>
+                        </div>
+                    </div>
+                    <div class="w-embed modal-body">
+                        <h1>Mon Parcours Professionnel</h1>
+                            <h3>Stage des écoles (2019)</h3>
+                                <p>Stage réalisé à la base navale de Toulon dans le secteur du numérique et du multimédia.</p>
+                            <h3>Alternance à l'école des sous-mariniers (SEPT. 2022 – SEPT. 2024)</h3>
+                                <p>Développeur de simulateurs en C#, Xaml (.Net 7.0) avec quelques notions de SQL dans un modèle MVVM. <br />
+                                    Participation à la création de simulateur de conduite de sous-marins nucléaire, <br/>
+                                    D’un logiciel de formation aux oreilles d’or, <br />
+                                    D’un serious game éducatif sur le métier de sous-marinier, <br />
+                                    Et d’un simulateur de visualisation de fluides. <br />
+                                    (Plus d'informations dans l'onglet "Projets")</p>
+                        <h1>Mon Parcours Scolaire</h1>
+                            <h3>Brevet des collèges (2019)</h3>
+                                <p>École privée Bon Accueil, France, Toulon, Var.</p>
+                            <h3>Baccalauréat (2022)</h3>
+                                <p> Baccalauréat général, option Math / Numérique et Sciences Informatiques. Lycée public Dumont d’Urville, France, Toulon, Var. </p>
+                            <h3> BTS SIO Option SLAM (SEPT. 2022 – SEPT. 2024)</h3>
+                                <p> BTS SIO option solutions logicielles et applications metiers, lycée public Bonaparte en alternance, France, Toulon, Var. </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="containercv2">
+                <div class="modal-wrapper">
+                    <div class="modal-header">
+                        <div class="button-circles-wrap"><a href="/" class="button-circle close w-inline-block"></a>
+                            <div class="button-circle"></div>
+                        </div>
+                        <div class="flex-center">
+                            <div>MesCompetences</div>
+                            <div>-project.html</div>
+                        </div>
+                    </div>
+                    <div class="w-embed modal-body">
+                        <h1>Compétences Acquises</h1>
+                        <img class="competences" src="../images/Competences.png" />
+                    </div>
+                </div>
+            </div>
+
+            <div class="containercv2">
+                <div class="modal-wrapper">
+                    <div class="modal-header">
+                        <div class="button-circles-wrap"><a href="/" class="button-circle close w-inline-block"></a>
+                            <div class="button-circle"></div>
+                        </div>
+                        <div class="flex-center">
+                            <div>MesActivites</div>
+                            <div>-project.html</div>
+                        </div>
+                    </div>
+                    <div class="w-embed modal-body">
+                        <h1>Activités</h1>
+                        <h3> Piano Jazz, Classique ; Saxophone ; Violon.
+                            Montage photo, vidéo, son. </h3>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        
+
+        <!-- Footer -->
+        <div class="footer">
+            <div class="container">
+                <div class="footer-row">
+                    <div class="footer-block">
+                        <div class="text-rights-short">©2024 Lemufi</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+        <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=5e87e737ee7085b9ba02c101"
+            type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous">
+        </script>
+        <script src="https://assets.website-files.com/5e87e737ee7085b9ba02c101/js/webflow.be692164d.js"
+            type="text/javascript"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js" type="text/javascript"></script>
+        <script src="../js/dropbar.js" type="text/javascript" </script>
 </body>
 </html>
